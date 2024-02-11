101a68bd3a83d2bb27d319c331ed032b9158a31b
tawansongsang <tawansongsang.k@gmail.com>
2024-02-11 21:58:51 +0700

    add missing features

--- crates/libs/lib-sqlserver/src/model/user_info/mod.rs
+++ crates/libs/lib-sqlserver/src/model/user_info/mod.rs
@@ -2,47 +2,47 @@ pub mod bmc;
 
 use serde::{Deserialize, Serialize};
 // use surrealdb::sql;
-use uuid::Uuid;
+use tiberius::{time::DateTime2, Uuid};
 
 #[derive(Debug, Deserialize)]
 pub struct UserInfo {
     // FIXME: change to sql server
-    // pub id: sql::Thing,
-    // pub username: String,
-    // pub email: String,
-    // pub email_verified: sql::Datetime,
-    // pub name: String,
+    // pub id: Uuid,
+    pub username: String,
+    pub name: String,
+    pub email: String,
+    // pub email_verified: DateTime2,
 
-    // // -- pwd and token info
-    // pub password: String,
+    // -- pwd and token info
+    pub password: String,
     // pub password_salt: Uuid,
     // pub token_salt: Uuid,
-    // pub create_by: sql::Thing,
-    // pub create_on: sql::Datetime,
-    // pub update_by: sql::Thing,
-    // pub update_on: sql::Datetime,
+    // pub create_by: Uuid,
+    // pub create_on: DateTime2,
+    // pub update_by: Uuid,
+    // pub update_on: Datetime2,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct UserInfoGet {
     // FIXME: change to sql server
-    // pub id: sql::Thing,
+    // pub id: Uuid,
     // pub username: String,
     // pub email: String,
-    // pub email_verified: sql::Datetime,
+    // pub email_verified: Datetime2,
     // pub name: String,
 
-    // pub create_by: sql::Thing,
-    // pub create_on: sql::Datetime,
-    // pub update_by: sql::Thing,
-    // pub update_on: sql::Datetime,
+    // pub create_by: Uuid,
+    // pub create_on: DateTime2,
+    // pub update_by: Uuid,
+    // pub update_on: DateTime2,
 }
 
 #[derive(Debug, Serialize)]
 pub struct UserInfoForCreate {
     pub username: String,
     // pub email: String,
-    // pub email_verified: sql::Datetime,
+    // pub email_verified: DateTime2,
     pub name: String,
     pub password: String,
 }
@@ -51,18 +51,18 @@ pub struct UserInfoForCreate {
 pub struct UserInfoCreated<'a> {
     pub username: &'a str,
     pub email: &'a str,
-    // pub email_verified: sql::Datetime,
+    // pub email_verified: DateTime2,
     pub name: String,
     pub password: String,
     // FIXME: change to sql server
-    // pub create_by: &'a Option<sql::Thing>,
-    // pub update_by: &'a Option<sql::Thing>,
+    // pub create_by: &'a Option<Uuid>,
+    // pub update_by: &'a Option<Uuid>,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct UserInfoForLogin {
     // FIXEME: change to sql server
-    // pub id: sql::Thing,
+    // pub id: Uuid,
     // pub username: String,
     // pub name: String,
     // pub password: Option<String>, // encrypted, #_scheme_id_#....
@@ -74,7 +74,7 @@ pub struct UserInfoForLogin {
 #[derive(Debug, Deserialize)]
 pub struct UserInfoForAuth {
     // FIXME: change to sql server
-    // pub id: sql::Thing,
+    // pub id: Uuid,
     // pub username: String,
 
     // // -- token info
@@ -84,5 +84,5 @@ pub struct UserInfoForAuth {
 #[derive(Debug, Deserialize)]
 pub struct UserInfoRecord {
     // FIXME: change to sql server
-    // pub id: sql::Thing,
+    // pub id: Uuid,
 }
